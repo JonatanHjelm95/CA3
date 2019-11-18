227fc48c897a4c78e2c60ccb067f27acd1ef458f
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2019-11-18 10:33:38 +0100

    new deploy 2

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ca3/precache-manifest.5105196ffcc7886051be05ebd5dacb16.js"
+  "/CA3/precache-manifest.21bf9c4635e44acd68b534e804b7d557.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ca3/index.html", {
+workbox.routing.registerNavigationRoute("/CA3/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
